c4f85c9202f6f3cb0b8bb88e5a224652ef95ddd6
zovi <zivazivi03@gmail.com>
2025-10-28 12:20:41 +0100

    popravek pri 2. vaji

--- Vaje2/main.js
+++ Vaje2/main.js
@@ -1,164 +1,131 @@
-// zagon svojega serverja: python3 -m http.server 3000
-// 3000 je številka porta, lahko daš poljubno (privzeta: 8000)
-
-const adapter = await navigator.gpu.requestAdapter(); // izberemo s katero grafično bomo delali
-const device = await adapter.requestDevice(); //dejanska incializacija
-
-
+// Initialize WebGPU
+const adapter = await navigator.gpu.requestAdapter();
+const device = await adapter.requestDevice();
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('webgpu');
-context.configure({
-    device: device,
-    format: 'rgba8unorm',
+const format = navigator.gpu.getPreferredCanvasFormat();
+context.configure({ device, format });
+
+// Create vertex buffer
+const vertices = new Float32Array([
+    // position    // color
+    -0.5, -0.5,    1, 0, 0, 1,
+     0.5, -0.5,    0, 1, 0, 1,
+    -0.5,  0.5,    0, 0, 1, 1,
+     0.5,  0.5,    1, 1, 0, 1,
+]);
+
+const vertexBuffer = device.createBuffer({
+    size: vertices.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
 });
 
-// pipeline: format vhodnih podatkov
-//     format izhodnih podatkov
-//     znotraj pipeline-a imamo pa ta 2 naša programčka - shaderja/senčilnika
+device.queue.writeBuffer(vertexBuffer, 0, vertices);
 
+// Create index buffer
+const indices = new Uint32Array([
+    // 1st triangle
+    0, 1, 2,
+    // 2nd triangle
+    2, 1, 3,
+]);
+
+const indexBuffer = device.createBuffer({
+    size: indices.byteLength,
+    usage: GPUBufferUsage.INDEX | GPUBufferUsage.COPY_DST,
+});
+
+device.queue.writeBuffer(indexBuffer, 0, indices);
+
+// Fetch and compile shaders
 const code = await fetch('shader.wgsl').then(response => response.text());
 const module = device.createShaderModule({ code });
 
+// Create the pipeline
 const vertexBufferLayout = {
-    arrayStride: 16,
-    attributes: [{
-        format: 'float32x4',
-        offset: 0,
-        shaderLocation: 0,
-    }]
-    
-
-};
-
-const colorBufferLayout = {
-    arrayStride: 16,
-    attributes: [{
-        format: 'float32x4',
-        offset: 0,
-        shaderLocation: 1,
-    }]
-    
-
+    arrayStride: 24,
+    attributes: [
+        {
+            shaderLocation: 0,
+            offset: 0,
+            format: 'float32x2',
+        },
+        {
+            shaderLocation: 1,
+            offset: 8,
+            format: 'float32x4',
+        },
+    ]
 };
 
 const pipeline = device.createRenderPipeline({
-    layout: 'auto',
     vertex: {
-        module: module,
-        buffers: [vertexBufferLayout, colorBufferLayout],
+        module,
+        entryPoint: 'vertex',
+        buffers: [vertexBufferLayout],
     },
     fragment: {
-        module: module,
-        targets: [{
-            format: 'rgba8unorm'
-        }]
+        module,
+        entryPoint: 'fragment',
+        targets: [{ format }],
     },
-
+    layout: 'auto',
 });
 
-// postions
-const vertices = new Float32Array ([ // podatki v glavnem pomnilniku
-    -0.5, -0.5, 0, 1,
-    0.5, -0.5, 0, 1,
-    -0.5, 0.5, 0, 1,
-    
-    0.5, -0.5, 0, 1,
-    -0.5, 0.5, 0, 1,
-    0.5, 0.5, 0, 1,
-
-]);
-
-// imamo alokacijo pomnilnika v grafični kartici
-const vertexBuffer = device.createBuffer({
-   size: 96,
-   usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
-
-});
-    
-
-// podatke moramo zdaj prenesti it gl. pom. v pomn. v graf. kartici
-device.queue.writeBuffer(vertexBuffer, 0, vertices);
-
-//colors
-const colors = new Float32Array ([ // podatki v glavnem pomnilniku
-    1, 0, 0, 1,
-    0, 1, 0, 1,
-    0, 0, 1, 1,
-    
-    0, 1, 0, 1,
-    0, 0, 1, 1,
-    1, 1, 0, 1,
-
-]);
-
-// imamo alokacijo pomnilnika v grafični kartici
-const colorBuffer = device.createBuffer({
-   size: 96,
-   usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
-
-});
-    
-// podatke moramo zdaj prenesti it gl. pom. v pomn. v graf. kartici
-device.queue.writeBuffer(colorBuffer, 0, colors);
-
-//matrix
-
+// Create uniform buffer
 const uniformBuffer = device.createBuffer({
-    size: 64,
+    size: 4 * 4 * 4,
     usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
-    
 });
 
+// Create the bind group
 const bindGroup = device.createBindGroup({
     layout: pipeline.getBindGroupLayout(0),
-    entries: [{
-        binding: 0,
-        resource: uniformBuffer,
-    }]
-    
+    entries: [
+        { binding: 0, resource: { buffer: uniformBuffer } },
+    ]
 });
 
-let x = -1;
-let y = -1;
+function update() {
+    // Animate the square
+    const time = performance.now() / 1000;
+    const radius = 0.5;
+    const frequency = 0.5;
+    const x = radius * Math.cos(frequency * time * 2 * Math.PI);
+    const y = radius * Math.sin(frequency * time * 2 * Math.PI);
 
-function frame() {
-
-    x += 0.01;
-    y += 0.01;
-    
     device.queue.writeBuffer(uniformBuffer, 0, new Float32Array([
         1, 0, 0, 0,
         0, 1, 0, 0,
         0, 0, 1, 0,
         x, y, 0, 1,
-        
     ]));
-    
-    const commandEncoder = device.createCommandEncoder();
+}
 
+function render() {
+    // Render the square
+    const commandEncoder = device.createCommandEncoder();
     const renderPass = commandEncoder.beginRenderPass({
         colorAttachments: [{
             view: context.getCurrentTexture().createView(),
             loadOp: 'clear',
+            clearValue: [0.7, 0.8, 0.9, 1],
             storeOp: 'store',
-            clearValue: [1, 0, 1, 1]
         }]
     });
-
-    renderPass.setPipeline(pipeline); //ne dela, ker imamo prazen pipeline
+    renderPass.setPipeline(pipeline);
     renderPass.setVertexBuffer(0, vertexBuffer);
-    renderPass.setVertexBuffer(1, colorBuffer);
+    renderPass.setIndexBuffer(indexBuffer, 'uint32');
     renderPass.setBindGroup(0, bindGroup);
-    renderPass.draw(6);
+    renderPass.drawIndexed(indices.length);
     renderPass.end();
-    requestAnimationFrame(frame);
     device.queue.submit([commandEncoder.finish()]);
-    
 }
+
+function frame() {
+    update();
+    render();
+    requestAnimationFrame(frame);
+}
+
 requestAnimationFrame(frame);
-
-
-
-
-
-
--- Vaje2/shader.wgsl
+++ Vaje2/shader.wgsl
@@ -1,28 +1,38 @@
-//rabimo 2 funkciji: vs in fs
+struct VertexInput {
+    @location(0) position: vec2f,
+    @location(1) color: vec4f,
+}
+
 struct VertexOutput {
     @builtin(position) position: vec4f,
-    @location(1) color: vec4f,
-    
+    @location(0) color: vec4f,
 }
 
-@group(0)
-@binding(0)
-var<uniform> matrix: mat4x4f;
+struct FragmentInput {
+    @location(0) color: vec4f,
+}
+
+struct FragmentOutput {
+    @location(0) color: vec4f,
+}
+
+@group(0) @binding(0) var<uniform> matrix: mat4x4f;
 
 @vertex
-//vrne ogljišča
-fn vertex(@location(0) position: vec4f, @location(1) color: vec4f) -> VertexOutput {
+fn vertex(input: VertexInput) -> VertexOutput {
     var output: VertexOutput;
 
-    output.position = matrix * position;
-    output.color = color;
+    output.position = matrix * vec4(input.position, 0, 1);
+    output.color = input.color;
+
     return output;
 }
-	
+
 @fragment
-//vrne barvo
-fn fragment(@location(1) color: vec4f) -> @location(0) vec4f {
-	return color;
+fn fragment(input: FragmentInput) -> FragmentOutput {
+    var output: FragmentOutput;
+
+    output.color = input.color;
+
+    return output;
 }
-
-
